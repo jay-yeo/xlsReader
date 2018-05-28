b8c74a7c2beafa38331035d1d66a43ee8a9c4901
Jason Yeo <jayyeo21@gmail.com>
2018-05-28 23:01:12 +0200

    Added exceptions to logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 import com.matritellabs.utama.readxls.ExcelReader;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import java.io.IOException;
 
 // xlsReader - https://github.com/jay-yeo/xlsReader
 public class Main {
 
     // Main method
-    public static void main(String[] args) throws IOException, InvalidFormatException {
+    public static void main(String[] args) {
 
         // Open .xls file
         ExcelReader xlsFile = new ExcelReader("tmp/files/jutott_neki.xls");
--- src/main/java/com/matritellabs/utama/readxls/ExcelReader.java
+++ src/main/java/com/matritellabs/utama/readxls/ExcelReader.java
@@ -1,44 +1,52 @@
 package com.matritellabs.utama.readxls;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
-import java.io.IOException;
 
 // ExcelReader class
 public class ExcelReader {
 
-    private static String filePath;
     private static Workbook xlsFile;
     private final static Logger log = LoggerFactory.getLogger(ExcelReader.class);
 
     // Constructor
-    public ExcelReader(String filePath) throws IOException, InvalidFormatException {
-        this.filePath = filePath;
-        setExcelFile();
+    public ExcelReader(String filePath) {
+        setExcelFile(filePath);
     }
 
 
-    // Open .xlsx file and set .xlsxFile field
-    public void setExcelFile() throws IOException, InvalidFormatException {
+    // Open Excel file and set .xls File field
+    public void setExcelFile(String filePath) {
         // Open .xls file
-        Workbook workbook = WorkbookFactory.create(new File(filePath));
+        try {
+            Workbook workbook = WorkbookFactory.create(new File(filePath));
 
-        this.xlsFile = workbook;
+            xlsFile = workbook;
 
-        // Log
-        log.info("New ExcelReader instance with " + filePath + " file");
+            // Log new file
+            log.info("New ExcelReader instance with " + filePath + " file");
+
+        } catch (Exception ex) {
+            // Log exception
+            log.error(ex.toString());
+        }
+
     }
 
     // Close .xls file
-    public void closeExcelFile() throws IOException {
-        // Close file
-        xlsFile.close();
-        // Log
-        log.info("Closing " + filePath);
+    public void closeExcelFile() {
+        try {
+            // Close file
+            xlsFile.close();
+
+            // Log close file
+            log.info("Closing file");
+        } catch (Exception ex) {
+            // Log exception
+            log.error(ex.toString());
+        }
     }
 
     // Read the first column of each worksheet
@@ -49,7 +57,7 @@ public class ExcelReader {
 
         // Loop through sheets and rows, printing results
         for(Sheet sheet: xlsFile) {
-            System.out.println("\nSHEET: " + sheet.getSheetName().toUpperCase());
+            System.out.println("\n" + sheet.getSheetName().toUpperCase());
             for (Row row: sheet) {
                 for(Cell cell: row) {
                     String cellValue = dataFormatter.formatCellValue(cell);
@@ -58,6 +66,10 @@ public class ExcelReader {
                 System.out.println();
             }
         }
+
+        // Log
+        log.info("Successfully printed results!");
+
 
     }
 
